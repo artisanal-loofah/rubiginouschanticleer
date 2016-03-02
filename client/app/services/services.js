4eff33502e4d33c19a1f56236e7e22b5079f0e18
Gar Lee <gardarr@gmail.com>
2016-03-02 11:04:23 -0800

    removed session factory from services

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,53 +1,4 @@
-angular.module( 'dinnerDaddy.services', [] )
-
-.factory( 'Session', function( $http, $window, $location ) {
-  return {
-    createSession: function( sessionName, callback ) {
-      return $http.post( '/api/sessions', { sessionName: sessionName } )
-      .then( function( response ) {
-        callback( sessionName ); // used for emitting session data
-        return response;
-      }, function( err ) {
-        console.error( err );
-      } );
-    },
-
-    fetchSessions: function() {
-      return $http.get ( '/api/sessions' )
-      .then( function( response ) {
-        return response.data;
-      }, function( err ) {
-        console.error( err );
-      } ); 
-    }, 
-
-    joinSession: function( sessionName, username, callback ) {
-      return $http.post( '/api/sessions/users', { sessionName: sessionName, username: username } )
-      .then( function( response ) {
-        callback( username, sessionName ); // used for emitting session data
-        $location.path( '/lobby' );
-        return response;
-      }, function( err ) {
-        console.error( err );
-      } );
-    },
-
-    setSession: function( sessionName ) {
-      $window.localStorage.setItem( 'sessionName', sessionName );
-    }, 
-
-    getSession: function() {
-      var sessionName = $window.localStorage.getItem( 'sessionName' );
-      return $http.get( '/api/sessions/' + sessionName )
-      .then( function( session ) {
-        return session.data;
-      }, function( err ) {
-        console.error( err );
-      });
-    }
-
-  }
-} )
+angular.module('dinnerDaddy.services', [])
 
 .factory( 'Match', function( $http, $location ) {
   return {
@@ -87,7 +38,7 @@ angular.module( 'dinnerDaddy.services', [] )
     }
 
   }
-} )
+})
 
 .factory( 'Socket', ['socketFactory', function(socketFactory){
   return socketFactory();
