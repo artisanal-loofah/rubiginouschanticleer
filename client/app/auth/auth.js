ab8b4b316445042b5981d63ca5b3228573073377
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-01 20:04:28 -0800

    Implement isAuth and signout methods

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,37 +1,37 @@
 angular.module( 'dinnerDaddy.auth', [] )
 
-.controller( 'AuthController', function($scope, Auth, $window, $location, $timeout) {
+.controller( 'AuthController', function($scope, $rootScope, Auth, $window, $location, $timeout) {
   $scope.user = {};
 
   if ($location.path() === '/signout') {
-    console.log( 'You are signed out. Redirecting in 2s.' );
+    console.log('You are signed out. Redirecting in 2s.');
     Auth.signout();
     $timeout( function() {
-      $location.path( '/signin' );
+      $location.path('/signin');
     }, 2000 );
   }
 
 
   $scope.signout = function () {
-    Auth.signout()
-    .then(function() {
-      // TODO: redirect
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
+    Auth.signout();
+    // might have to reload page as well, not sure yet
+    $location.path('/signin');
   };
 
 })
 
-.factory( 'Auth', function($http, $location, $window) {
+.factory( 'Auth', function($http, $cookies, $location, $window) {
   var isAuth = function() {
-    // TODO: check for user cookies
+    return ($cookies.get('fbId') !== undefined);
   };
 
   var signout = function() {
-    // TODO: remove cookies
+    //clear cookies
+    $cookies.remove("name");
+    $cookies.remove("picture");
+    $cookies.remove("fbId");
   };
+
   return {
     isAuth: isAuth,
     signout: signout
