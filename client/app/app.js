2e70199d91a73ae2a43f99b42cc00e9a5b551dce
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-01 16:26:45 -0800

    Comment out jwt token attach

--- client/app/app.js
+++ client/app/app.js
@@ -50,29 +50,29 @@ var app = angular.module( 'moviematch', [
       redirectTo: '/signin'
     })
 
-    $httpProvider.interceptors.push('AttachTokens');
+    // $httpProvider.interceptors.push('AttachTokens');
 
 })
 
-.factory('AttachTokens', function ($window) {
-  var attach = {
-    request: function (object) {
-      var jwt = $window.localStorage.getItem('com.moviematch');
-      if (jwt) {
-        object.headers['x-access-token'] = jwt;
-      }
-      object.headers['Allow-Control-Allow-Origin'] = '*';
-      return object;
-    }
-  };
-  return attach;
-})
+// .factory('AttachTokens', function ($window) {
+//   var attach = {
+//     request: function (object) {
+//       var jwt = $window.localStorage.getItem('com.moviematch');
+//       if (jwt) {
+//         object.headers['x-access-token'] = jwt;
+//       }
+//       object.headers['Allow-Control-Allow-Origin'] = '*';
+//       return object;
+//     }
+//   };
+//   return attach;
+// })
 
-.run(function ($rootScope, $location, Auth) {
-  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
-      $location.path('/signin');
-    }
-  });
-});
+// .run(function ($rootScope, $location, Auth) {
+//   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+//     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+//       $location.path('/signin');
+//     }
+//   });
+// });
 
