ac3fb66c92c6f8438950bae2f0e420e9cbdd8e0e
Albert Huynh <alberthuynh91@gmail.com>
2016-03-01 18:27:00 -0800

    More updates to yelp api. Loaded restaurants

--- client/app/match/match.js
+++ client/app/match/match.js
@@ -10,7 +10,7 @@ angular.module( 'dinnerDaddy.match', ['dinnerDaddy.services'] )
   $scope.currRestaurant;
 
   Session.getSession()
-  .then( function( session ) {
+  .then(function (session) {
     $scope.session = session;
   });
 
