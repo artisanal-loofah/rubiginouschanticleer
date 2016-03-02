490a4d1f94a6cf983d73831de1a64fd226a54b39
Gar Lee <gardarr@gmail.com>
2016-03-01 19:53:54 -0800

    setup friendships model

---
+++ server/users/friendships.js
@@ -0,0 +1,20 @@
+var db = require( '../config/db');
+var Sequelize = require( 'sequelize' );
+var User = require('./users');
+
+var Friendship = db.define('friendships', {
+  user_id: Sequelize.INTEGER,
+  friend_fb_id: Sequelize.STRING
+});
+
+Friendship.sync().then(function() {
+  console.log("friendships table created");
+})
+.catch( function(err) {
+  console.error(err);
+});
+
+
+Friendship.belongsTo(User, {foreignKey: 'id'});
+
+module.exports = Friendship;
